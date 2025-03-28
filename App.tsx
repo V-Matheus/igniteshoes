8187fb36b61f1460fcf036519298a2fbf2291679
Victor Matheus <victormatheus507@gmail.com>
2025-03-27 21:09:20 -0300

    optendo a opcao selecionada

--- App.tsx
+++ App.tsx
@@ -12,8 +12,9 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
-import { OneSignal } from 'react-native-onesignal';
+import { NotificationClickEvent, OneSignal } from 'react-native-onesignal';
 import { tagUserInfoCreate } from './src/notifications/notificationsTags';
+import { act, useEffect } from 'react';
 
 const oneSignalAppId =
   Platform.OS === 'ios' ? 'iosID' : 'ecedc784-f334-42fb-9be5-b5afc94af3e9';
@@ -24,7 +25,36 @@ OneSignal.Notifications.requestPermission(true);
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-  tagUserInfoCreate()
+  tagUserInfoCreate();
+
+  useEffect(() => {
+    console.log('App mounted');
+    
+    const handleNotificationClick = (event: NotificationClickEvent) => {
+      const { actionId } = event.result;
+
+      switch (actionId) {
+        case '1':
+          console.log('Ver todos');
+          break;
+
+        case '2':
+          console.log('Ver Pedido');
+          break;
+        default:
+          console.log('Nenhum botão pressionado');
+          break;
+      }
+    };
+
+    OneSignal.Notifications.addEventListener('click', handleNotificationClick);
+
+    return () =>
+      OneSignal.Notifications.removeEventListener(
+        'click',
+        handleNotificationClick,
+      );
+  }, []);
 
   return (
     <NativeBaseProvider theme={THEME}>
